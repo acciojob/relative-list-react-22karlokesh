0b4add8ede0416a6b351c40d009a52bbce66a64b
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-04 13:51:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div id="main">
+//                {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
 
 const App = () => {
+  const relatives = ['Uncle', 'Aunt', 'Cousin', 'Grandparents', 'Brother'];
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {relatives.map((name, index) => (
+          <li key={`relativeListItem${index + 1}`}>{name}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
